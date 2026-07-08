bac1f469ea6ea3ce2c19d558288265d7a3951b6a
Aakanksha2812 <shindeaakanksha28@gmail.com>
2026-07-08 17:16:15 +0530

    updated ResumeService logic

--- resume-service/resume-service/src/main/java/com/resumeproject/resume_service/service/ResumeService.java
+++ resume-service/resume-service/src/main/java/com/resumeproject/resume_service/service/ResumeService.java
@@ -5,24 +5,34 @@ import com.resumeproject.resume_service.model.Resume;
 import com.resumeproject.resume_service.repository.ResumeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ResumeService {
-    private static final Logger log = LoggerFactory.getLogger(ResumeService.class);
-    private final ResumeRepository repo;
+
+    private final ResumeRepository resumeRepository;
     private final ResumeEventPublisher resumeEventPublisher;
 
-    public Resume uploadResume(String candidateName, String candidateEmail, String fileName, String fileContent) {
-        Resume resume = Resume.builder().candidateName(candidateName).candidateEmail(candidateEmail).fileName(fileName).fileContent(fileName).status("UPLOADED").build();
-        Resume savedResume = repo.save(resume);
+    public Resume uploadResume(String candidateName,
+                               String candidateEmail,
+                               String fileName,
+                               String fileContent) {
+
+        // 1. Resume MongoDB mein save karo
+        Resume resume = Resume.builder()
+                .candidateName(candidateName)
+                .candidateEmail(candidateEmail)
+                .fileName(fileName)
+                .fileContent(fileContent)
+                .status("UPLOADED")
+                .build();
+
+        Resume savedResume = resumeRepository.save(resume);
         log.info("Resume saved to MongoDB with id: {}", savedResume.getId());
 
-
+        // 2. Kafka pe event publish karo
         resumeEventPublisher.publishResumeUploadEvent(savedResume.getId());
 
         return savedResume;
