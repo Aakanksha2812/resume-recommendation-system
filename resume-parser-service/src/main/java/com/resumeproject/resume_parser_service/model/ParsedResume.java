811b273fd14571532aef4ed17e620e002ad165b8
Aakanksha2812 <shindeaakanksha28@gmail.com>
2026-07-10 11:43:04 +0530

    created model class for ResumeParserServiceApplication

---
+++ resume-parser-service/src/main/java/com/resumeproject/resume_parser_service/model/ParsedResume.java
@@ -0,0 +1,78 @@
+package com.resumeproject.resume_parser_service.model;
+
+import java.util.List;
+
+public class ParsedResume {
+    private String id;
+    private String resumeId;
+    private String candidateName;
+    private String candidateEmail;
+    private String yearOfExp;
+    private String education;
+    private List<String> extractedSkill;
+    private String status;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getResumeId() {
+        return resumeId;
+    }
+
+    public void setResumeId(String resumeId) {
+        this.resumeId = resumeId;
+    }
+
+    public String getCandidateName() {
+        return candidateName;
+    }
+
+    public void setCandidateName(String candidateName) {
+        this.candidateName = candidateName;
+    }
+
+    public String getCandidateEmail() {
+        return candidateEmail;
+    }
+
+    public void setCandidateEmail(String candidateEmail) {
+        this.candidateEmail = candidateEmail;
+    }
+
+    public String getYearOfExp() {
+        return yearOfExp;
+    }
+
+    public void setYearOfExp(String yearOfExp) {
+        this.yearOfExp = yearOfExp;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public List<String> getExtractedSkill() {
+        return extractedSkill;
+    }
+
+    public void setExtractedSkill(List<String> extractedSkill) {
+        this.extractedSkill = extractedSkill;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
