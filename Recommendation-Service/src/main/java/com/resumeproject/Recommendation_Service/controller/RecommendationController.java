e1813ba3760779789c6413bbfb88c041b2878334
Aakanksha2812 <shindeaakanksha28@gmail.com>
2026-07-18 18:56:57 +0530

    added recommendation controller

--- Recommendation-Service/src/main/java/com/resumeproject/Recommendation_Service/controller/RecommendationController.java
+++ Recommendation-Service/src/main/java/com/resumeproject/Recommendation_Service/controller/RecommendationController.java
@@ -1,39 +1,36 @@
 package com.resumeproject.Recommendation_Service.controller;
 
-import com.resumeproject.Recommendation_Service.kafka.RecommendationEventPublisher;
 import com.resumeproject.Recommendation_Service.model.Recommendation;
 import com.resumeproject.Recommendation_Service.repository.RecommendationRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/recommendation")
+@RequestMapping("/recommendations")
 public class RecommendationController {
-    private static final Logger log = LoggerFactory.getLogger(RecommendationEventPublisher.class);
 
+    private static final Logger log = LoggerFactory.getLogger(RecommendationController.class);
     private final RecommendationRepository recommendationRepository;
 
     public RecommendationController(RecommendationRepository recommendationRepository) {
         this.recommendationRepository = recommendationRepository;
     }
 
-    @GetMapping("/email")
-    public ResponseEntity<List<Recommendation>> getRecommendation(@PathVariable String email) {
-        log.info("fetching recommendation email {} ", email);
+
+    @GetMapping("/{email}")
+    public ResponseEntity<List<Recommendation>> getRecommendations(@PathVariable String email) {
+        log.info("Fetching recommendations for: {}", email);
         List<Recommendation> recommendations = recommendationRepository.findByCandidateEmail(email);
         return ResponseEntity.ok(recommendations);
-
     }
 
+
     @GetMapping
-    public ResponseEntity<List<Recommendation>> getAllRecommendation(@PathVariable String email) {
+    public ResponseEntity<List<Recommendation>> getAllRecommendations() {
         List<Recommendation> recommendations = recommendationRepository.findAll();
         return ResponseEntity.ok(recommendations);
     }
