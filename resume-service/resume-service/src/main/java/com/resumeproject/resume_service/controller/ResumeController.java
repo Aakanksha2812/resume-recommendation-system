b12ab41508578a32d15826b2b7ba236dacca9a1d
Aakanksha2812 <shindeaakanksha28@gmail.com>
2026-07-19 19:21:36 +0530

    Pdf uploading functionality developed from backend

--- resume-service/resume-service/src/main/java/com/resumeproject/resume_service/controller/ResumeController.java
+++ resume-service/resume-service/src/main/java/com/resumeproject/resume_service/controller/ResumeController.java
@@ -2,10 +2,15 @@ package com.resumeproject.resume_service.controller;
 
 import com.resumeproject.resume_service.model.Resume;
 import com.resumeproject.resume_service.service.ResumeService;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/resume")
@@ -23,8 +28,20 @@ public class ResumeController {
             @RequestParam String candidateName,
             @RequestParam String candidateEmail,
             @RequestParam String fileName,
-            @RequestParam String fileContent) {
+            @RequestParam MultipartFile file) {
         log.info("Received resume upload request for: {}", candidateEmail);
+        String fileContent = "";
+        try {
+            log.info("extracting details from pdf");
+            PDDocument document = PDDocument.load(file.getInputStream());
+            PDFTextStripper stripper = new PDFTextStripper();
+            fileContent = stripper.getText(document);
+            document.close();
+            log.info("pdf text added file content {}", fileContent.length());
+        } catch (IOException io) {
+            log.error("error while extracting text from pdf  {}", io.getMessage());
+            return ResponseEntity.badRequest().build();
+        }
         Resume resume = resumeService.uploadResume(candidateName, candidateEmail, fileName, fileContent);
         return ResponseEntity.ok(resume);
     }
